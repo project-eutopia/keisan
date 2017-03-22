c31792a16c61f2c21392ac687a22906171ef26ed
Christopher Locke <project.eutopia@gmail.com>
2017-03-22 00:40:28 -0700

    Add activesupport and pry dependencies

--- lib/symbolic_math.rb
+++ lib/symbolic_math.rb
@@ -1,3 +1,6 @@
+require "active_support"
+require "active_support/core_ext"
+
 require "symbolic_math/version"
 
 module SymbolicMath
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,6 @@
 require "bundler/setup"
 require "symbolic_math"
+require "pry"
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
