dedbadd2b361530ffdbe132f02f2a4b0bc136c51
Christopher Locke <project.eutopia@gmail.com>
2017-03-22 21:50:25 -0700

    Allow focus: true to work

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,4 +9,11 @@ RSpec.configure do |config|
   config.expect_with :rspec do |c|
     c.syntax = :expect
   end
+
+  # These two settings work together to allow you to limit a spec run
+  # to individual examples or groups you care about by tagging them with
+  # `:focus` metadata. When nothing is tagged with `:focus`, all examples
+  # get run.
+  config.filter_run :focus
+  config.run_all_when_everything_filtered = true
 end
