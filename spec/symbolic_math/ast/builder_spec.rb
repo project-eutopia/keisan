125d251d1ac6f4691c26ee9afa07f10be404cd18
Christopher Locke <project.eutopia@gmail.com>
2017-03-22 17:52:47 -0700

    Add parentheses test for Builder

--- spec/symbolic_math/ast/builder_spec.rb
+++ spec/symbolic_math/ast/builder_spec.rb
@@ -4,13 +4,14 @@ RSpec.describe SymbolicMath::AST::Builder do
   context "simple operations" do
     it "correctly builds the AST" do
       operations = {
-        "1 + 2"        => eq(3),
-        "7.5 - 3"      => eq(4.5),
-        "2 + 3 * 5"    => eq(17),
-        "8 / 5"        => eq(Rational(8,5)),
-        "4**2 + 3 * 5" => eq(31),
-        "2 ** (1/2)"   => eq(Math.sqrt(2)),
-        "sin(pi)"      => be_within(1e-10).of(0)
+        "1 + 2"               => eq(3),
+        "7.5 - 3"             => eq(4.5),
+        "2 + 3 * 5"           => eq(17),
+        "8 / 5"               => eq(Rational(8,5)),
+        "(1+2) * (3+4*(1+1))" => eq(33),
+        "4**2 + 3 * 5"        => eq(31),
+        "2 ** (1/2)"          => eq(Math.sqrt(2)),
+        "sin(pi)"             => be_within(1e-10).of(0)
       }
 
       operations.each do |operation, matcher|
