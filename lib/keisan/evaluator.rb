a0714209e3865be243b1bc41db7290c6eff7969d
Christopher Locke <project.eutopia@gmail.com>
2017-03-25 00:16:35 -0700

    Update to handle definitions with definitions

--- lib/keisan/evaluator.rb
+++ lib/keisan/evaluator.rb
@@ -24,37 +24,28 @@ module Keisan
     private
 
     def variable_evaluate(expression, definitions = {})
-      unless definitions.empty?
-        raise Keisan::Exceptions::InvalidExpression.new("Cannot use local definitions for definition expression") unless definitions.empty?
-      end
-
       match = expression.match VARIABLE_DEFINITION
       name = match[1]
       expression = match[2]
       ast = Keisan::AST::Builder.new(string: expression).ast
 
-      context = calculator.context
+      context = calculator.context.spawn_child(definitions)
       unbound_variables = ast.unbound_variables(context)
       unless unbound_variables <= Set.new([name])
         raise Keisan::Exceptions::InvalidExpression.new("Unbound variables found in variable definition")
       end
 
-      context.register_variable!(name, ast.value(context))
+      calculator.context.register_variable!(name, ast.value(context))
     end
 
     def function_evaluate(expression, definitions = {})
-      unless definitions.empty?
-        raise Keisan::Exceptions::InvalidExpression.new("Cannot use local definitions for definition expression") unless definitions.empty?
-      end
-
       match = expression.match FUNCTION_DEFINITION
       name = match[1]
       args = match[2].split(",").map(&:strip)
       expression = match[3]
       ast = Keisan::AST::Builder.new(string: expression).ast
 
-      context = calculator.context
-
+      context = calculator.context.spawn_child(definitions)
       unbound_variables = ast.unbound_variables(context)
       unbound_functions = ast.unbound_functions(context)
       # Ensure the variables are contained within the arguments
@@ -62,39 +53,25 @@ module Keisan
         raise Keisan::Exceptions::InvalidExpression.new("Unbound variables found in function definition")
       end
 
-      context.register_function!(
+      calculator.context.register_function!(
         name,
         Proc.new do |*received_args|
           unless args.count == received_args.count
             raise Keisan::Exceptions::InvalidFunctionError.new("Invalid number of arguments for #{name} function")
           end
 
-          local = context.spawn_child
+          local = calculator.context.spawn_child(definitions)
           args.each.with_index do |arg, i|
             local.register_variable!(arg, received_args[i])
           end
+
           ast.value(local)
         end
       )
     end
 
     def pure_evaluate(expression, definitions = {})
-      local = calculator.context.spawn_child
-
-      definitions.each do |name, value|
-        case value
-        when Proc
-          local.register_function!(name, value)
-        else
-          local.register_variable!(name, value)
-        end
-      end
-
-      evaluate_expression(expression, local)
-    end
-
-    def evaluate_expression(expression, context)
-      Keisan::AST::Builder.new(string: expression).ast.value(context)
+      Keisan::AST::Builder.new(string: expression).ast.value(calculator.context.spawn_child(definitions))
     end
   end
 end
