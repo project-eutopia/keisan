452fcd0f06fd150bfc5893800e52ca25f5c5a2d0
Christopher Locke <project.eutopia@gmail.com>
2017-03-24 18:47:47 -0700

    Bump version to 0.2.1

--- spec/keisan_spec.rb
+++ spec/keisan_spec.rb
@@ -3,6 +3,6 @@ require "spec_helper"
 RSpec.describe Keisan do
   it "has a version number" do
     expect(Keisan::VERSION).not_to be nil
-    expect(Keisan::VERSION).to eq "0.2.0"
+    expect(Keisan::VERSION).to eq "0.2.1"
   end
 end
