d1fdf0c1b210125d20987097b82c6c80e27a1e6e
Christopher Locke <project.eutopia@gmail.com>
2017-04-03 02:46:00 -0700

    Update simplify for "if" function

--- lib/keisan/ast/functions/if.rb
+++ lib/keisan/ast/functions/if.rb
@@ -7,6 +7,8 @@ module Keisan
             raise Keisan::Exceptions::InvalidFunctionError.new("Require 2 or 3 arguments to if")
           end
 
+          context ||= Context.new
+
           bool = children[0].value(context)
 
           if bool
@@ -16,8 +18,24 @@ module Keisan
           end
         end
 
+        def simplify(context = nil)
+          context ||= Context.new
+          @children = children.map {|child| child.simplify(context)}
+
+          if children[0].is_a?(AST::Boolean)
+            if children[0].value
+              children[1]
+            else
+              # If no third argument, then children[2] gives nil, and to_node makes this AST::Null
+              children[2].to_node
+            end
+          else
+            self
+          end
+        end
+
         def unbound_functions(context = nil)
-          context ||= Keisan::Context.new
+          context ||= Context.new
 
           children.inject(Set.new) do |res, child|
             res | child.unbound_functions(context)
