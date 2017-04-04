ddfe05b9be823aa9b2fa359e73ce89220e535b41
Christopher Locke <project.eutopia@gmail.com>
2017-04-03 18:39:25 -0700

    Update version to 0.4.0

--- spec/keisan_spec.rb
+++ spec/keisan_spec.rb
@@ -3,6 +3,6 @@ require "spec_helper"
 RSpec.describe Keisan do
   it "has a version number" do
     expect(Keisan::VERSION).not_to be nil
-    expect(Keisan::VERSION).to eq "0.3.0"
+    expect(Keisan::VERSION).to eq "0.4.0"
   end
 end
