33e79aba9d99b564e52df781a27f9886c3861b08
Christopher Locke <project.eutopia@gmail.com>
2017-12-11 15:35:02 +0900

    Add spec to validate README code examples

---
+++ spec/readme_spec.rb
@@ -0,0 +1,48 @@
+require "spec_helper"
+
+RSpec.describe "README.md" do
+  describe "code examples" do
+    File.open(File.expand_path("../../README.md", __FILE__)) do |file|
+      content = file.read
+      content.scan(/^```ruby$\n(?<block>(?:^.*?$\n)*?)^```$/m).map {|match| match[0].split("\n").map(&:strip)}
+    end.each.with_index do |code_block, i|
+      it "runs code example #{i} correctly: #{code_block}" do
+        b = binding
+
+        outputs = code_block.map do |line|
+          begin
+            eval(line, b)
+          rescue Keisan::Exceptions::BaseError => e
+            e
+          end
+        end
+
+        expectations = code_block.map do |line|
+          match = line.match(/\A\#\=\>(.+)\z/)
+          if match
+            match2 = match[1].match(/Keisan::Exceptions::(.+?): (.+)/)
+            if match2
+              Keisan::Exceptions.const_get(match2[1]).new(match2[2])
+            else
+              eval match[1], b
+            end
+          else
+            nil
+          end
+        end
+
+        expectations.each.with_index do |expectation, i|
+          case expectation
+          when Keisan::Exceptions::BaseError
+            expect(outputs[i-1].class).to eq expectation.class
+            expect(outputs[i-1].message).to eq expectation.message
+          when NilClass, FalseClass
+            # Nothing
+          else
+            expect(outputs[i-1]).to eq expectation
+          end
+        end
+      end
+    end
+  end
+end
