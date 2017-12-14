b0c63482635eb03298cca7d98eca67c8d00e782a
Christopher Locke <project.eutopia@gmail.com>
2017-12-14 19:48:05 +0900

    Improve UnaryOperator coverage

--- spec/keisan/ast/unary_identity_spec.rb
+++ spec/keisan/ast/unary_identity_spec.rb
@@ -8,4 +8,29 @@ RSpec.describe Keisan::AST::UnaryIdentity do
       expect(ast.simplified.name).to eq "x"
     end
   end
+
+  describe "value" do
+    it "eliminates UnaryIdentity" do
+      ast = Keisan::AST::UnaryIdentity.new([Keisan::AST::Number.new(12)])
+      expect(ast.value).to eq 12
+    end
+  end
+
+  describe "evaluate" do
+    it "eliminates UnaryIdentity" do
+      ast = Keisan::AST::UnaryIdentity.new([Keisan::AST::Number.new(12)])
+      expect(ast.evaluate.value).to eq 12
+    end
+  end
+
+  describe "differentiate" do
+    it "eliminates UnaryIdentity" do
+      ast = Keisan::AST::UnaryIdentity.new([Keisan::AST::Function.new(Keisan::AST::Variable.new("x"), "f")])
+      diff = ast.differentiate(Keisan::AST::Variable.new("x"))
+      expect(diff).to be_a(Keisan::AST::Function)
+      expect(diff.name).to eq "diff"
+      expect(diff.children.first).to be_a(Keisan::AST::Function)
+      expect(diff.children.first.name).to eq "f"
+    end
+  end
 end
