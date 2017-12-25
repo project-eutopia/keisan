93dbb64eaaaaaa091810d7948e12cde0174c3189
Christopher Locke <project.eutopia@gmail.com>
2017-12-25 18:14:29 +0900

    Add Cell node, which just encapsulates a node

---
+++ lib/keisan/ast/cell.rb
@@ -0,0 +1,56 @@
+module Keisan
+  module AST
+    class Cell < Node
+      attr_reader :node
+
+      def initialize(node)
+        @node = node
+      end
+
+      def unbound_variables(context = nil)
+        node.unbound_variables(context)
+      end
+
+      def unbound_functions(context = nil)
+        node.unbound_functions(context)
+      end
+
+      def deep_dup
+        dupped = dup
+        dupped.instance_variable_set(
+          :@node,
+          dupped.node.deep_dup
+        )
+        dupped
+      end
+
+      def value(context = nil)
+        node.value(context)
+      end
+
+      def evaluate(context = nil)
+        node.evaluate(context)
+      end
+
+      def simplify(context = nil)
+        node.simplify(context)
+      end
+
+      def evaluate_assignments(context = nil)
+        node.evaluate_assignments(context)
+      end
+
+      def differentiate(variable, context = nil)
+        node.differentiate(variable, context)
+      end
+
+      def replace(variable, replacement)
+        node.replace(variable, replacement)
+      end
+
+      def to_s
+        node.to_s
+      end
+    end
+  end
+end
---
+++ spec/keisan/ast/cell_spec.rb
@@ -0,0 +1,28 @@
+require "spec_helper"
+
+RSpec.describe Keisan::AST::Cell do
+  describe "delegation of methods" do
+    let(:node) { Keisan::AST::Node.new }
+    let(:cell) { described_class.new(node) }
+
+    %i(unbound_variables unbound_functions value evaluate simplify evaluate_assignments to_s).each do |method|
+      it "delegates #{method} to internal node" do
+        expect(node).to receive(method)
+        cell.send(method)
+      end
+    end
+
+    it "delegates differentiate to internal node" do
+      variable = Keisan::AST::Variable.new("x")
+      expect(node).to receive(:differentiate).with(variable, nil)
+      cell.differentiate(variable, nil)
+    end
+
+    it "delegates replace to internal node" do
+      variable = Keisan::AST::Variable.new("x")
+      number = Keisan::AST::Number.new(5)
+      expect(node).to receive(:replace).with(variable, number)
+      cell.replace(variable, number)
+    end
+  end
+end
