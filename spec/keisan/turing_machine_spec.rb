d4ae5449ee0bb78324681ba8a6564cb663fcc59e
Christopher Locke <project.eutopia@gmail.com>
2017-12-26 14:57:33 +0900

    Update Turing machine spec to use block of text

--- spec/keisan/turing_machine_spec.rb
+++ spec/keisan/turing_machine_spec.rb
@@ -4,37 +4,56 @@ RSpec.describe "Turing machine" do
   let(:calculator) { Keisan::Calculator.new }
 
   before do
-    calculator.evaluate("tape_positive = [0]")
-    calculator.evaluate("tape_negative = []")
-    calculator.evaluate("tape_index(index) = if (index >= 0, index, index.abs() - 1)")
-    calculator.evaluate("tape_ensure_space(index) = { if (index >= 0, { while (index >= tape_positive.size, tape_positive = tape_positive + [0]); tape_positive[index] }, { let index = index.abs() - 1; while (index >= tape_negative.size, tape_negative = tape_negative + [0]); tape_negative[index] }) }")
-    calculator.evaluate("tape_value(index) = {tape_ensure_space(index); if (index >= 0, tape_positive[tape_index(index)], tape_negative[tape_index(index)])}")
+    calculator.evaluate(<<-KEISAN
+                         tape_positive = [0]
+                         tape_negative = []
+                         tape_index(index) = if (index >= 0, index, index.abs() - 1)
 
-    calculator.evaluate("
-      run_machine(machine) = {
-        state = 0;
-        index = 0;
+                         tape_ensure_space(index) = {
+                           if (index >= 0, {
+                             while (index >= tape_positive.size, tape_positive = tape_positive + [0])
+                             tape_positive[index]
+                           }, {
+                             let index = index.abs() - 1
+                             while (index >= tape_negative.size, tape_negative = tape_negative + [0])
+                             tape_negative[index]
+                           })
+                         }
 
-        running = true;
+                         tape_value(index) = {
+                           tape_ensure_space(index)
+                           if (index >= 0, {
+                             tape_positive[tape_index(index)]
+                           }, {
+                             tape_negative[tape_index(index)]
+                           })
+                         }
 
-        while(running, {
-          value = tape_value(index);
-          commands = machine[state][value]
-          let abs_index = tape_index(index)
+                         run_machine(machine) = {
+                           state = 0
+                           index = 0
 
-          if (index >= 0,
-            tape_positive[abs_index] = commands[0]
-          ,
-            tape_negative[abs_index] = commands[0]
-          );
+                           running = true
 
-          index = index + commands[1];
-          state = commands[2];
+                           while(running, {
+                             value = tape_value(index)
+                             commands = machine[state][value]
+                             let abs_index = tape_index(index)
 
-          if (state < 0, running = false);
-        })
-      }"
-    )
+                             if (index >= 0,
+                               tape_positive[abs_index] = commands[0]
+                             ,
+                               tape_negative[abs_index] = commands[0]
+                             )
+
+                             index = index + commands[1]
+                             state = commands[2]
+
+                             if (state < 0, running = false)
+                           })
+                         }
+    KEISAN
+                       )
   end
 
   it "3-state busy beaver" do
