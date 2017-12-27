5f1b51d47a181ecf2b5ca910b58a3d437349ad23
Christopher Locke <project.eutopia@gmail.com>
2017-12-27 15:10:08 +0900

    Separate interactive shell from interpreter

--- lib/keisan/interpreter.rb
+++ lib/keisan/interpreter.rb
@@ -7,10 +7,26 @@ module Keisan
     end
 
     def run(file_name)
-      content = File.open(file_name) do |file|
+      if file_name.nil?
+        run_from_stdin
+      else
+        run_from_file(file_name)
+      end
+    end
+
+    private
+
+    def run_from_stdin
+      run_on_content STDIN.read
+    end
+
+    def run_from_file(file_name)
+      run_on_content File.open(file_name) do |file|
         file.read
       end
+    end
 
+    def run_on_content(content)
       calculator.evaluate(content)
     end
   end
