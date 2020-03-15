dc8d5fb5716fd67ba0b38b7fa0f406dddbd3877f
Christopher Locke <project.eutopia@gmail.com>
2020-03-14 18:47:39 -0700

    Update version to 0.8.1

--- spec/keisan_spec.rb
+++ spec/keisan_spec.rb
@@ -2,7 +2,7 @@ require "spec_helper"
 
 RSpec.describe Keisan do
   it "has the expected version number" do
-    expect(Keisan::VERSION).to eq "0.8.0"
+    expect(Keisan::VERSION).to eq "0.8.1"
   end
 
   context "module methods" do
