63ecc6bb89e3d2d8876adff6897d2cae0f8dd476
Christopher Locke <project.eutopia@gmail.com>
2020-07-15 21:49:43 +0900

    Add additional test for complex comments

--- spec/keisan/calculator_spec.rb
+++ spec/keisan/calculator_spec.rb
@@ -17,6 +17,7 @@ RSpec.describe Keisan::Calculator do
     expect(calculator.evaluate("# Hello world")).to eq nil
     expect(calculator.evaluate("2 + 3 # 4")).to eq 5
     expect(calculator.evaluate("# Initial comment\n 1+2\n 123 # Real result\n# Trailing comment\n")).to eq 123
+    expect(calculator.evaluate("# Blah )(('\"\n 'foo' + '#bar' #Ignore")).to eq "foo#bar"
   end
 
   it "can handle custom functions" do
