43c5ab4508788232cbea878ecf3208fedd0b2d15
Christopher Locke <project.eutopia@gmail.com>
2024-03-04 11:51:23 +0900

    Update to version 0.9.2

--- spec/version_spec.rb
+++ spec/version_spec.rb
@@ -2,6 +2,6 @@ require "spec_helper"
 
 RSpec.describe Keisan do
   it "has the expected version number" do
-    expect(Keisan::VERSION).to eq "0.9.1"
+    expect(Keisan::VERSION).to eq "0.9.2"
   end
 end
